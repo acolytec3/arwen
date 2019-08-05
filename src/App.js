49754e956105851dd094f7cc5cc8d47b57b535e9
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-08-05 09:24:59 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+//import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
+import Web3Provider, { useWeb3Context, Web3Consumer } from "web3-react";
+import { ethers } from "ethers";
+import { abi } from "./PublicResolver"
+import connectors from "./Connectors.js";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3Provider connectors={connectors} libraryName="ethers.js">
+      <div className="App">
+        <ActivateConnectors/>
+        <Arweave />
+      </div>
+    </Web3Provider>
   );
 }
 
+function ActivateConnectors() {
+  const context = useWeb3Context();
+
+  console.log(context);
+
+  if (context.error) {
+    console.error("Error!");
+  }
+
+  const [transactionHash, setTransactionHash] = React.useState();
+
+  function sendTransaction() {
+    const signer = context.library.getSigner();
+
+    signer
+      .sendTransaction({
+        to: ethers.constants.AddressZero,
+        value: ethers.utils.bigNumberify("0")
+      })
+      .then(({ hash }) => {
+        setTransactionHash(hash);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <h1>ENS Arweave Explorer</h1>
+
+      <Web3ConsumerComponent />
+
+      {context.error && (
+        <p>An error occurred, check the console for details.</p>
+      )}
+
+      {Object.keys(connectors).map(connectorName => (
+        <button
+          key={connectorName}
+          disabled={context.connectorName === connectorName}
+          onClick={() => context.setConnector(connectorName)}
+        >
+          Activate {connectorName}
+        </button>
+      ))}
+
+      <br />
+      <br />
+
+      {(context.active || (context.error && context.connectorName)) && (
+        <button onClick={() => context.unsetConnector()}>
+          {context.active ? "Deactivate Connector" : "Reset"}
+        </button>
+      )}
+
+      {context.active && context.account && !transactionHash && (
+        <button onClick={sendTransaction}>Send Dummy Transaction</button>
+      )}
+
+      {transactionHash && <p>{transactionHash}</p>}
+
+    </React.Fragment>
+  );
+}
+
+function Web3ConsumerComponent() {
+  return (
+    <Web3Consumer>
+      {context => {
+        const { active, connectorName, account, networkId } = context;
+        return (
+          active && (
+            <React.Fragment>
+              <p>Active Connector: {connectorName}</p>
+              <p>Account: {account || "None"}</p>
+              <p>Network ID: {networkId}</p>
+            </React.Fragment>
+          )
+        );
+      }}
+    </Web3Consumer>
+  );
+}
+
+function Arweave () {
+  const context = useWeb3Context();
+
+  const [arweaveURL, setarweaveURL] = React.useState()
+
+  function getArweave ()
+  {
+    var nameHash = ethers.utils.namehash('acolytec3.test')
+    const publicResolver = new ethers.Contract('0x5FfC014343cd971B7eb70732021E26C35B744cc4', abi, context.library)
+    publicResolver.text(nameHash,'url')
+    .then(link => {
+      console.log(link)
+      setarweaveURL(link)
+    })
+  }
+  if (context.error) {
+    console.error("Error!");
+    return <p>Error</p>
+  }
+  if (context.active){
+    /*context.library.resolveName('acolytec3.text')
+    .then(resolvedAddress => {
+      console.log(resolvedAddress)
+      //var reverseLookup = context.library.lookupAddress(resolvedAddress)
+      //console.lookup(reverseLookup)
+    })*/ 
+    return (
+      <React.Fragment>
+        {context.active && !arweaveURL && (
+        <button onClick={getArweave}>Retrieve Arweave URL</button>
+      )}
+        {arweaveURL && <p>{arweaveURL}</p>}
+      </React.Fragment>
+    )
+  }
+  else return <p>Connection not active</p>
+}
 export default App;
---
+++ src/Connectors.js
@@ -0,0 +1,73 @@
+import { Connectors } from "web3-react";
+//import TrezorApi from "trezor-connect";
+//import WalletConnectApi from "@walletconnect/web3-subprovider";
+//import FortmaticApi from "fortmatic";
+//import PortisApi from "@portis/web3";
+
+const {
+  InjectedConnector,
+  NetworkOnlyConnector,
+ // TrezorConnector,
+ // LedgerConnector,
+ // WalletConnectConnector,
+ // FortmaticConnector,
+ // PortisConnector
+} = Connectors;
+
+const supportedNetworkURLs = {
+  1: "https://mainnet.infura.io/v3/",
+  3: "https://ropsten.infura.io/v3/",
+  4: "https://rinkeby.infura.io/v3/"
+};
+
+const defaultNetwork = 1;
+
+const Injected = new InjectedConnector({
+  supportedNetworks: [1, 3, 4]
+});
+
+const Network = new NetworkOnlyConnector({
+  providerURL: supportedNetworkURLs[1]
+});
+/*
+const Trezor = new TrezorConnector({
+  api: TrezorApi,
+  supportedNetworkURLs,
+  defaultNetwork,
+  manifestEmail: "",
+  manifestAppUrl: ""
+});
+
+const Ledger = new LedgerConnector({
+  supportedNetworkURLs,
+  defaultNetwork
+});
+
+const WalletConnect = new WalletConnectConnector({
+  api: WalletConnectApi,
+  bridge: "https://bridge.walletconnect.org",
+  supportedNetworkURLs,
+  defaultNetwork
+});
+
+const Fortmatic = new FortmaticConnector({
+  api: FortmaticApi,
+  apiKey: "",
+  logoutOnDeactivation: false
+});
+
+const Portis = new PortisConnector({
+  api: PortisApi,
+  dAppId: "",
+  network: "mainnet"
+});
+*/
+export default {
+  Injected,
+  Network,
+/*  Trezor,
+  Ledger,
+  WalletConnect,
+  Fortmatic,
+  Portis*/
+};
