49e444de503f94f1ac3dae210027a538910518bf
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-08-09 16:11:33 -0400

    Add ENS Domain Registration feature (#10)
    Got ENS working!

--- src/components/ENSRegistrationComponent.js
+++ src/components/ENSRegistrationComponent.js
@@ -3,22 +3,32 @@ import { ethers } from "ethers";
 import { useWeb3Context } from "web3-react";
 import { registrarAbi } from "../Registrar.js"
 import { Navbar, Form, FormControl, InputGroup, Button } from 'react-bootstrap';
-
+import { ethControllerAbi } from '../EthController.js'
 
 function ENSRegistrationComponent() {
   const context = useWeb3Context();
   const [ensSubDomainName, setEnsSubDomainName] = React.useState()
+  const [ensDomainName, setEnsDomainName] = React.useState()
 
-  const handleChange = evt => {
+  const handleEnsSubDomainChange = evt => {
     setEnsSubDomainName(evt.target.value)
     console.log('Setting ENS Domain Name to ' + ensSubDomainName)
   }
 
-  const handleSubmit = (evt) => {
+  const handleEnsSubdomainSubmit = (evt) => {
     evt.preventDefault();
     registerEnsSubDomain()
   }
 
+  const handleEnsDomainChange = evt => {
+    setEnsDomainName(evt.target.value)
+    console.log('Setting ENS Domain Name to ' + ensDomainName)
+  }
+
+  const handleEnsDomainSubmit = (evt) => {
+    evt.preventDefault();
+    registerEnsDomain()
+  }
   async function registerEnsSubDomain()
   {
     const signer = context.library.getSigner()
@@ -35,29 +45,65 @@ function ENSRegistrationComponent() {
 
   }
 
+  async function registerEnsDomain()
+  {
+    const signer = context.library.getSigner()
+    console.log("ENS Domain Name Hash is " + ensDomainName)
+    const ethController = new ethers.Contract('0x357DBd063BeA7F0713BF88A3e97B7436B0235979', ethControllerAbi, signer)
+    var domainName = ensDomainName.split('.')[0]
+    var txid = await ethController.available(domainName)
+    console.log(txid)
+    if (txid === false) {
+      return console.log('Domain name unavailable')
+    }
+    var commitSecret = ethers.utils.formatBytes32String(domainName)
+    var commitmentHash = await ethController.makeCommitment(domainName, signer._address, commitSecret)
+    console.log("The commit hash for this registration request is " + commitmentHash + " and the commit secret is " + commitSecret)
+    var rentPrice = await ethController.rentPrice(domainName, 31535999)
+    rentPrice = rentPrice * 1.05
+    console.log('ENS Domain Name Rent Price: ' + rentPrice)
+    txid = await ethController.commit(commitmentHash)
+    console.log(txid)
+    await txid.wait()
+    var minCommitTimeBN = await ethController.minCommitmentAge()
+    var minCommitTime = minCommitTimeBN.toNumber()
+    await new Promise(resolve => setTimeout(resolve, minCommitTime*1000))
+    console.log('Waited ' + minCommitTime + ' seconds')
+    var gasPrice = ethers.providers.getGasPrice()
+    console.log(gasPrice.toNumber())
+    txid = ethController.register(domainName, signer._address, ethers.utils.bigNumberify(31535999), commitSecret, { value: rentPrice, gasLimit: 300000, gasPrice: gasPrice })
+    .then(txid => console.log(txid))
+    .catch(error => console.log(error))
+ }
+
+
   if (context.active){
   return (
     <Navbar className="bg-light justify-content-between">
-      <Form inline>
+      <Form inline onSubmit={handleEnsDomainSubmit}>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+            <InputGroup.Text id="basic-addon1">EBS Domain Name</InputGroup.Text>
           </InputGroup.Prepend>
           <FormControl
-            placeholder="ENS Domain"
+            placeholder="alice.eth"
             aria-label="ENSDomain"
+            type='text'
+            value={ensDomainName}
+            onChange={handleEnsDomainChange}
             aria-describedby="basic-addon1"
           />
         </InputGroup>
+        <Button type="submit">Register Domain</Button>
       </Form>
-      <Form inline onSubmit={handleSubmit}>
+      <Form inline onSubmit={handleEnsSubdomainSubmit}>
         <FormControl
           type="text"
           value={ensSubDomainName}
-          onChange={handleChange}
-          placeholder="ENS SubDomain"
+          onChange={handleEnsSubDomainChange}
+          placeholder="bob.alice.eth"
           className=" mr-sm-2" />
-        <Button type="submit">Register</Button>
+        <Button type="submit">Register Subdomain</Button>
       </Form>
     </Navbar>
   )}
