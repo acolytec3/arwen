213381358f7b605df909cf5be736779f2cf21f85
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-08-11 15:10:28 -0400

    Fix ENS registration

--- src/components/ENSRegistrationComponent.js
+++ src/components/ENSRegistrationComponent.js
@@ -49,7 +49,6 @@ function ENSRegistrationComponent() {
   async function registerEnsDomain()
   {
     const signer = context.library.getSigner()
-    const provider = ethers.getDefaultProvider()
     console.log("ENS Domain Name Hash is " + ensDomainName)
     const ethController = new ethers.Contract('0x357DBd063BeA7F0713BF88A3e97B7436B0235979', ethControllerAbi, signer)
     var domainName = ensDomainName.split('.')[0]
@@ -62,34 +61,22 @@ function ENSRegistrationComponent() {
     var commitmentHash = await ethController.makeCommitment(domainName, signer._address, commitSecret)
     console.log("The commit hash for this registration request is " + commitmentHash + " and the commit secret is " + commitSecret)
     var rentPrice = await ethController.rentPrice(domainName, 31535999)
-    rentPrice = rentPrice * 1.05
     console.log('ENS Domain Name Rent Price: ' + rentPrice)
     txid = await ethController.commit(commitmentHash)
-    setEnsSpinner({state:'Commmitting',per: 33})
-    console.log("Commitment transaction for ENS domain name " + ensDomainName + " with transaciton hash " + txid)
+    console.log(txid)
     await txid.wait()
     var minCommitTimeBN = await ethController.minCommitmentAge()
     var minCommitTime = minCommitTimeBN.toNumber()
     await new Promise(resolve => setTimeout(resolve, minCommitTime*1000))
-    console.log('Waited ' + minCommitTime + ' seconds') 
-    var gasPrice = await provider.getGasPrice()
-    console.log(gasPrice)
-    txid = ethController.register(domainName, signer._address, ethers.utils.bigNumberify(31535999), commitSecret, { value: rentPrice, gasLimit: 500000, gasPrice: 500 })
-    .then(txid => {
-      console.log("Transaction submitted to register " + ensDomainName + " with hash " + txid)
-      setEnsSpinner({state:'Registering',per: 66})
-    })
-    .catch(error => {
-      console.log(error)
-      setEnsSpinner('error')
-    })
+    console.log('Waited ' + minCommitTime + ' seconds')
+    txid = await ethController.register(domainName, signer._address, ethers.utils.bigNumberify(31535999), commitSecret, { value: rentPrice, gasLimit: 300000, gasPrice: 15 })
+    await txid.wait()
+    console.log("Registered " + ensDomainName + " with transaction " + txid)
     const registrar = new ethers.Contract('0x112234455c3a32fd11230c42e7bccd4a84e02010', registrarAbi, signer)
     txid = await registrar.setResolver(ethers.utils.namehash(ensDomainName),'0x5FfC014343cd971B7eb70732021E26C35B744cc4')
-    console.log("Setting Resolver with transaction hash " + txid)
-    txid.wait()
-    setEnsSpinner({state:'Done',per: 100})
-    await new Promise(resolve => setTimeout(resolve, 10000))
-    setEnsSpinner({state:'Not Started', per: 0})
+    await txid.wait()
+    console.log(txid)
+
  }
 
   if (context.active){
