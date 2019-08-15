e7931cd39c9aa4e0f93b4d7ebf7852c8fd831a8c
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-08-15 19:58:38 -0400

    Updates

---
+++ precache-manifest.e97d8af19b84e8950279d37b4ab6b162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5a1ce46cdaac2013a599ec894dfbfa",
+    "url": "/arwen/index.html"
+  },
+  {
+    "revision": "ab91323a3a37c4022f6f",
+    "url": "/arwen/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "eb18a50b3e52e0e3d3cc",
+    "url": "/arwen/static/js/2.f6b73be3.chunk.js"
+  },
+  {
+    "revision": "ab91323a3a37c4022f6f",
+    "url": "/arwen/static/js/main.0c5b33b0.chunk.js"
+  },
+  {
+    "revision": "b4376a4dce25a5049e56",
+    "url": "/arwen/static/js/runtime~main.e3db19f7.js"
+  }
+]);
