fd17e4d2ef443acb1ae874c34050aaece9d2d4ef
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-08-27 20:15:22 -0400

    Updates

---
+++ precache-manifest.a319dc50b772f441d8857d165e2e77d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9268cdb78453a9846222b4d717014ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd13570dcce8d304955e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e0be9a993d5903a8b62d",
+    "url": "/static/js/2.3344bae8.chunk.js"
+  },
+  {
+    "revision": "bd13570dcce8d304955e",
+    "url": "/static/js/main.9696006e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arwen/precache-manifest.57dcd3f011d5e950c570117e5ee2963e.js"
+  "/precache-manifest.a319dc50b772f441d8857d165e2e77d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arwen/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
